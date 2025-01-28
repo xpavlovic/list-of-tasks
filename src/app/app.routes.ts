3de2e3d75bbdf6a0910ff98498a8cc09f60cdb50
Danijela <daky98@gmail.com>
2025-01-28 22:40:22 +0100

    initial setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'task-list', component: TaskListComponent },
+];
